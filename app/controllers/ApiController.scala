7e4ae8b7b5191f9b085881d84ff34cf5c7ef7dbf
Alfredo <alfredotorre@gmail.com>
2019-03-07 09:34:23 +0000

    Add API and configurations

---
+++ app/controllers/ApiController.scala
@@ -0,0 +1,19 @@
+// app/controllers/ApiController.scala
+
+// Make sure it's in the 'controllers' package
+package controllers
+
+import javax.inject.{Inject, Singleton}
+import play.api.mvc.{AbstractController, ControllerComponents}
+
+@Singleton
+class ApiController @Inject()(cc: ControllerComponents)
+  extends AbstractController(cc) {
+
+  // Create a simple 'ping' endpoint for now, so that we
+  // can get up and running with a basic implementation
+  def ping = Action { implicit request =>
+    Ok("PONG!")
+  }
+
+}
